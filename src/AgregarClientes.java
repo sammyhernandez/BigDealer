47c49a2c454d8f32a2485dc5997c81f21f670dd0
Sammy Hernandez <Master-pro@windowslive.com>
2017-10-01 21:54:27 +0200

    Se modifico el filtro del formulario por que estaba presentando un inconveniente con los cliente.

--- src/AgregarClientes.java
+++ src/AgregarClientes.java
@@ -13,11 +13,7 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.MaskFormatter;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 
 /**
