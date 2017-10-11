2ad392cd22ec6a3f5ee56b8c9f1187956079c63b
Sammy Hernandez <Master-pro@windowslive.com>
2017-10-11 02:29:59 +0200

    El formulario proveedor esta casi completo

--- src/Proveedor.java
+++ src/Proveedor.java
@@ -10,11 +10,7 @@ import java.util.Date;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
